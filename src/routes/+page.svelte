edcd7d3d5bdd7d2843551957187858be13c81735
Marius Vaitkus <marius@chronosphere.io>
2023-07-22 18:03:10 +0300

    Add row collor based on wind speed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,22 +15,61 @@
 		currentSpot = $page.url.hash;
 		forecastPromise = loadForecast(currentSpot);
 	}
+
+    // calculates the background color based on the wind speed
+    const style = (wind: number) => {
+        // 3bft 4.5 - 0, 255, 255
+        // 4bft 6.7 - 0, 255, 0
+        // 5bft 9.35 - 255, 255, 0
+        // 6bft 12.3 - 255, 0, 0
+        // 7bft 15.5 - 255, 0, 255
+        // 8bft 18.95 - 255, 0, 255
+        // return background color based on windspeed
+        let r = 0;
+        let g = 0;
+        let b = 0;
+        let opacity = "15%"
+        if (wind < 4.5) { // 0, 255, 255
+            r = 0;
+            g = 255;
+            b = 255;
+            opacity = `${(wind / 4.5) * 15}%`;
+            return `background-color: rgba(${r}, ${g}, ${b}, ${opacity})`
+        };
+        if (wind < 6.7) { // 0, 255, 0
+            r = 0;
+            g = 255;
+            b = 255 - ((wind - 4.5) / 2.2) * 255;
+            return `background-color: rgba(${r}, ${g}, ${b}, ${opacity})`
+        }
+        if (wind < 9.35) { // 255, 255, 0
+            r = ((wind - 6.7) / 2.65) * 255;
+            g = 255;
+            b = 0;
+            return `background-color: rgba(${r}, ${g}, ${b}, ${opacity})`
+        }
+        if (wind < 12.3) { // 255, 0, 0
+            r = 255;
+            g = 255 - ((wind - 9.35) / 2.95) * 255;
+            b = 0;
+            return `background-color: rgba(${r}, ${g}, ${b}, ${opacity})`
+        }
+        if (wind < 15.5) { // 255, 0, 255
+            r = 255;
+            g = 0;
+            b = ((wind - 12.3) / 3.2) * 255;
+            return `background-color: rgba(${r}, ${g}, ${b}, ${opacity})`
+        }
+        if (wind >= 15.5) {
+            r = 255;
+            g = 0;
+            b = 255;
+            return `background-color: rgba(${r}, ${g}, ${b}, ${opacity})`
+        }
+    }
 </script>
 
 <div class="container">
-	<!-- <figure> -->
-	<!-- <nav>
-			<ul>
-				{#each Object.entries(spotConfig) as [spot, { name }]}
-                {#if spot == currentSpot}
-					<li><a href="{base}/{spot}">{name}</a></li>
-                {:else}
-                    <li><a href="{base}/{spot}">{name}</a></li>
-                {/if}
-				{/each}
-			</ul>
-		</nav> -->
-	<!-- </figure> -->
 	<div class="grid">
 		{#each Object.entries(spotConfig) as [spot, { name }]}
 			{#if spot == currentSpot}
@@ -57,13 +96,13 @@
 			<h2>{day.dateStr}</h2>
 			<table>
 				{#each day.items as item}
-					<tr>
+					<tr style={style(item.wind)}>
 						<td>{item.time}</td>
 						<td><img src={item.symbolUrl} alt="weather symbol" width="40" height="40" /></td>
 						<td>{item.temperature.toFixed(0)}º</td>
 						<td>
 							{item.wind.toFixed(1)} ({item.gusts.toFixed(0)}) m/s
-                            <!-- 10.0 (12.0) m/s -->
+							<!-- 10.0 (12.0) m/s -->
 							<svg
 								style="transform: rotate({item.direction}deg);"
 								x="0"
@@ -88,14 +127,14 @@
 	.grid {
 		grid-template-columns: repeat(3, minmax(0%, 1fr));
 	}
-    @media (min-width: 992px) {
+	@media (min-width: 992px) {
 		.grid {
 			grid-template-columns: repeat(auto-fit, minmax(0%, 1fr));
 		}
 	}
-    /* override end */
+	/* override end */
 	.active {
 		text-decoration: underline;
-        text-decoration-color: var(--primary);
+		text-decoration-color: var(--primary);
 	}
 </style>
